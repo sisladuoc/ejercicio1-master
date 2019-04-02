c6d548ad4a0db8ec4e1ca85e95d328e73cf081a1
cetecom <cetecom@LCV10823>
2019-04-01 21:39:01 -0300

    Cambios lunes 01/04

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+    include('lib/constantes.php');
+?>
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <link href="css/estiloprincipal.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+        <div id="titulo">
+            <div id="imag"></div>
+            <h2>Mi sitio</h2>
+        </div>
+        <div id="menu"><?php include('menu.php'); ?></div>
+        <div id="contenido">
+            <h2>Titulo pagina</h2>
+            <p>
+                contenido
+            </p>
+        </div>
+
+    </body>
+
+    <script>
+        $("#minrgesos").on("click", function (event) {
+            if (($("[id*=iproductos]").css("display") != "none")) {
+                $("[id*=iproductos]").css("display", "none");
+                $("[id*=iventas]").css("display", "none");
+                $("[id*=ibodega]").css("display", "none");
+            } else {
+                $("[id*=iproductos]").show();
+                $("[id*=iproductos]").css("display", "block");
+                $("[id*=iventas]").show();
+                $("[id*=iventas]").css("display", "block");
+                $("[id*=ibodega]").show();
+                $("[id*=ibodega]").css("display", "block");
+            }
+        });
+        $("#minformes").on("click", function (event) {
+            if (($("[id*=infventas]").css("display") != "none")) {
+                $("[id*=infventas]").css("display", "none");
+            } else {
+                $("[id*=infventas]").show();
+                $("[id*=infventas]").css("display", "block");
+            }
+        });
+        $("#mconsultas").on("click", function (event) {
+            if (($("[id*=cproductos]").css("display") != "none")) {
+                $("[id*=cproductos]").css("display", "none");
+            } else {
+                $("[id*=cproductos]").show();
+                $("[id*=cproductos]").css("display", "block");
+            }
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+    </script>
+</html>
---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class="list-group">
+<a  class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-2x"></i> Ventas Express</a>
+<a id="minrgesos" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Ingresos"><i class="fa fa-usd fa-2x"></i> Ingresos</a>
+<a id="iproductos" class="list-group-item submenu" href="<?=URLBASE;?>ingresos/productos.php" ><i class="fa fa-sun-o fa-2x"></i> Productos</a>
+<a id="iventas" class="list-group-item submenu" href="<?=URLBASE;?>ingresos/ventas.php" ><i class="fa fa-sun-o fa-2x"></i> Ventas</a>
+<a id="ibodega" class="list-group-item submenu" href="<?=URLBASE;?>ingresos/bodega.php" ><i class="fa fa-sun-o fa-2x"></i> Bodega</a>
+<a id="minformes" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Informes"><i class="fa fa-book fa-2x"></i> Informes</a>
+<a id="infventas" class="list-group-item submenu" href="<?=URLBASE;?>informes/ventas.php" ><i class="fa fa-sun-o fa-2x"></i> Ventas</a>
+<a id="mconsultas" class="list-group-item" href="#" data-toggle="tooltip" data-placement="right" title="Consultas"><i class="fa fa-search fa-2x"></i> Consultas</a>
+<a id="cproductos" class="list-group-item submenu" href="<?=URLBASE;?>consultas/productos.php" ><i class="fa fa-sun-o fa-2x"></i> Productos</a>
+</div>
+      
